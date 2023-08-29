93c8a6fa352724c1b5db008ee22d6e08f2a069d1
PNH1112 <hoangphn@metropolia.fi>
2023-08-29 13:34:18 +0300

    push

---
+++ mathInput.py
@@ -0,0 +1,6 @@
+
+a = float(input("Please enter the value of acceleration"))
+b = float(input("Please enter the mass of an object"))
+result = a*b
+print("the weight of an object is:", result, "kg.m/s2")
+
