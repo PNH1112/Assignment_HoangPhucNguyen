1ad2964b6a0e625bc09b5e1e1984feb8617d1e30
PNH1112 <hoangphn@metropolia.fi>
2023-08-29 09:26:55 +0300

    push

--- 2.py
+++ 2.py
@@ -7,3 +7,13 @@ rad = input("What is radius of circle?")
 r = int(rad)
 result = math.pi*r**2
 print(f"The area of the circle: , {result:2.2f}")
+
+import math
+length = input("What is the length of the rectangle?")
+l = int(length)
+width = input("What is the width of the rectangle?")
+w = int(width)
+pe = (l+w)*2
+print("The perimeter of the rectangle is:", pe)
+area = (l*w)
+print("The area of the rectangle is:", area)
