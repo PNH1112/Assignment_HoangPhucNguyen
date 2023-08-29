1681ba0864c2062393bc7f354f3544dcd3d5c7a8
PNH1112 <142902879+PNH1112@users.noreply.github.com>
2023-08-29 14:42:19 +0300

    Delete mathInput.py

--- mathInput.py
+++
@@ -1,6 +0,0 @@
-
-a = float(input("Please enter the value of acceleration"))
-b = float(input("Please enter the mass of an object"))
-result = a*b
-print("the weight of an object is:", result, "kg.m/s2")
-.
