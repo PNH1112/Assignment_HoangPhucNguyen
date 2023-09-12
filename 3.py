49e6fd5b8e1899612fd26aaf51f43c05b10d45ae
PNH1112 <142902879+PNH1112@users.noreply.github.com>
2023-09-12 08:08:29 +0300

    3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,13 @@
-
+#1
 zander = float(input("How is the length of your zander?"))
 if zander < 42:
     print("You must release the fish back into the lake. Your zander must be 42cm or more.")
 else:
     print("Every thing is ok, you can go.")
 
+#2
 cabin = input("What is your cabin class?")
-
-if cabin == "LUX" or cabin =="lux" or cabin =="Lux":
+if cabin == "LUX" or cabin =="lux":
     print("Cabin class description : upper-deck cabin with a balcony")
 elif cabin == "A" or cabin == "a":
     print("Cabin class description : above the car deck, equipped with a window")
@@ -18,6 +18,7 @@ elif cabin == "C" or cabin =="c":
 else:
     print("Invalid cabin class")
 
+#3
 fe_ma = input("What is your gender?")
 if fe_ma == "FEMALE":
     ask = float(input("What are the value of your biological?"))
@@ -32,8 +33,12 @@ else:
     else:
         print("Go to the hospital now.")
 
+#4
 i = int(input("Enter year"))
 if i % 4 == 0 and (i % 100 == 0 and i % 400 == 0):
     print("This year is a leap year")
 else:
     print("This year is not a leap year")
+
+
+
