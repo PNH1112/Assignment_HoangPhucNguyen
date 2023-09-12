69b77779e3964370ca2cfa092a3e6133ddfb7469
PNH1112 <142902879+PNH1112@users.noreply.github.com>
2023-09-12 08:07:58 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,13 +1,16 @@
 
+#1
 name = input("What's your name?")
 print("Hello,", name)
 
+#2
 import math
 rad = input("What is radius of circle?")
 r = float(rad)
 result = math.pi*r**2
 print(f"The area of the circle: , {result:2.2f}")
 
+#3
 import math
 length = input("What is the length of the rectangle?")
 l = float(length)
@@ -18,6 +21,7 @@ print("The perimeter of the rectangle is:", pe)
 area = (l*w)
 print("The area of the rectangle is:", area)
 
+#4
 a = float(input("What is the integer no.1?"))
 b = float(input("What is the integer no.2?"))
 c = float(input("What is the integer no.3?"))
@@ -28,6 +32,7 @@ print(f"Sum of those are:, {sum:2.2f}")
 print(f"Product of those are:, {product:2.2f}")
 print(f"Average of those are:, {average:2.2f}")
 
+#5
 ta = float(input("Enter talents"))
 po = float(input("Enter pounds"))
 lo = float(input("Enter lots"))
@@ -42,6 +47,7 @@ gram = gr % 1000
 print("The weight in modern units")
 print(f"{kilo} kilograms and, {gram:5.2f} grams")
 
+#6
 import random
 no = int(input("Choose 3 or 4 digit code?"))
 if no == 3:
