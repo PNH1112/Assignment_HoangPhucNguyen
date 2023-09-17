f6b9fec99573cee0df23a02b63692c57d73b5a92
PNH1112 <142902879+PNH1112@users.noreply.github.com>
2023-09-17 09:25:21 +0300

    Add files via upload

---
+++ 6..py
@@ -0,0 +1,79 @@
+
+#1
+import random
+def all():
+    dice = random.randint(1, 6)
+    list = []
+    while True:
+        dice = random.randint(1, 6)
+        list.append(dice)
+        if dice ==6:
+            break
+    print(list)
+    return list
+all()
+
+#2
+import random
+def maxdice():
+    max = int(input("Enter the number of sides: "))
+    list = []
+    while True:
+        dice = random.randint(1, max)
+        list.append(dice)
+        if dice == max:
+            break
+    print(list)
+    return list
+maxdice()
+
+#3
+def convert():
+    while True:
+        gallon = float(input("Enter the volume of gallon need to be converted(input negative number to stop): "))
+        lit = gallon * 3.78541
+        if gallon < 0:
+            print("Bye")
+            break
+        print(f"{gallon} gallon(s) converts to {lit:.2f} lit(es)")
+    return lit
+convert()
+
+#4
+list = [100,200,300,1,16,123]
+def all(list):
+    a = sum(list)
+    print(a)
+    return a
+all(list)
+
+#5
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def all(list):
+    list1 = []
+    for i in list:
+        if i%2 == 0:
+            list1.append(i)
+    print(list1)
+    return list1
+all(list)
+
+#6
+#d is the diameter of the pizza(cm)
+#p is the price of the pizza
+#u is the unit of the pizza(price/m^2)
+import math
+def pizza(d,p):
+    r = (d/2)/100
+    s = math.pi*r*r
+    u = p/s
+    return u
+pizza1 = pizza(30,10)
+pizza2 = pizza(29,8.8)
+if pizza1 < pizza2:
+    print(f"the unit price of pizza 1 is ({pizza1:.2f}) lower than the unit price of pizza 2 ({pizza2:.2f})")
+elif pizza2 < pizza1:
+    print(f"the unit price of pizza 2 is ({pizza2:.2f}) lower than the unit price of pizza 1 ({pizza1:.2f})")
+else:
+    print("the unit price of 2 pizzas is the same")
+
